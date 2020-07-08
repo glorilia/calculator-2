a8d6a102538bbc4ef9b83bb5ef5ecc516448f069
Glorili Alejandro <gloriliale@gmail.com>
2020-07-08 23:10:21 +0000

    Added addition functionality

--- calculator.py
+++ calculator.py
@@ -7,7 +7,15 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     equation = input("Enter your equation > ")
     tokens = equation.split()
-    if tokens[0] == 'q':
+    operator = tokens[0]
+    num1 = float(tokens[1])
+    if len(tokens) > 2 :
+        num2 = float(tokens[2])
+
+    if operator == 'q':
         print("You will exit.")
         break
+    else:
+        if operator == '+':
+            print(add(num1, num2))
 
