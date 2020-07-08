cd6e8e019e704cd343c559e715aa11d6953ac76e
Glorili Alejandro <gloriliale@gmail.com>
2020-07-08 23:15:35 +0000

    Added subtraction functionality

--- calculator.py
+++ calculator.py
@@ -8,14 +8,16 @@ while True:
     equation = input("Enter your equation > ")
     tokens = equation.split()
     operator = tokens[0]
+    if operator == 'q':
+        print("You will exit.")
+        break
+
     num1 = float(tokens[1])
     if len(tokens) > 2 :
         num2 = float(tokens[2])
 
-    if operator == 'q':
-        print("You will exit.")
-        break
-    else:
-        if operator == '+':
-            print(add(num1, num2))
+    if operator == '+':
+        print(add(num1, num2))
+    if operator == '-':
+        print(subtract(num1, num2))
 
